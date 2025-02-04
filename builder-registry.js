7055b478c90260625d267016d06550b3289b2ccd
SheemaBuilder <sheemaparwaz@gmail.com>
2025-02-04 21:21:18 +0530

    initial commit

---
+++ builder-registry.js
@@ -0,0 +1,33 @@
+import { builder, Builder } from "@builder.io/react";
+import dynamic from "next/dynamic";
+
+builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY);
+
+Builder.registerComponent(
+  dynamic(() => import("./components/Counter/Counter")),
+  {
+    name: "Counter",
+  }
+);
+
+
+Builder.register("editor.settings", {
+  styleStrictMode: true, // optional
+  designTokens: {
+    colors: [
+      { name: "Brand Red", value: "var(--red, #ff0000)" },
+      { name: "Brand Blue", value: "rgba(93, 150, 255, 1)" },
+      { name: "White", value: "rgb(255, 255, 255)" },
+      { name: "black", value: "rgb(0, 0, 0)" },
+    ],
+    spacing: [
+      { name: "Large", value: "var(--space-large, 20px)" },
+      { name: "Small", value: "var(--space-small, 10px)" },
+      { name: "Tiny", value: "5px" },
+    ],
+    fontFamily: [
+      { name: 'Serif Font', value: 'var(--serif-font, Times, serif)' },
+      { name: 'Primary Font', value: 'Roboto, sans-serif' },
+    ]
+  },
+});
